7e9988853dc4032fb631455937877914c084c28a
Chandu J S <app.coder@outlook.in>
2020-05-01 12:21:55 +0530

    cached db variable added

--- helpers/db.js
+++ helpers/db.js
@@ -6,13 +6,17 @@ const Outgoing = require('../models/outgoing')
 const Incoming = require('../models/incoming')
 const Notification = require('../models/notification')
 
+let conn = null
+
 module.exports = async () => {
-  const conn = mongoose.connect(process.env.MONGODB_URI, {
-    bufferCommands: false,
-    bufferMaxEntries: 0,
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  if (!conn) {
+    conn = mongoose.connect(process.env.MONGODB_URI, {
+      bufferCommands: false,
+      bufferMaxEntries: 0,
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+  }
   await conn
 
   return { Device, Outgoing, Incoming, Notification }
